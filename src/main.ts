63b5640139306304a060d3078073929b81589558
Ingrid Stanic <ingridstanic@gmail.com>
2025-11-13 11:18:17 +0100

    added utils

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
+import { FavGames } from './models/FavGames'
 import './style.css'
+import { createHtmlForTopGames, createHTMLHeader } from './utils';
+
+const rdr2 = new FavGames ("Red Dead Redemption 2", "Rockstar Games", "2018", "Open World, Western, Action-adventure, Non-linear", "10+++", "433h");
+const cp2077 = new FavGames ("Cyberpunk 2077", "CD Project RED", "2020", "RPG, Open world, First-person shooter, Sci-Fi", "8.9", "209h");
+const lou2 = new FavGames ("Last of Us Part II", "Naughty Dog", "2020", "Action-adventure, Survival horror, Story-driven, Thrid-person shooter", "9.2", "152h");
+const swo = new FavGames ("Starwars Outlaws", "Massive Entertainment", "2024", "Action-adventure, Open World, Third-person shooter, Sci-Fi", "8.2", "131h");
+const swjs = new FavGames ("Starwars Jedi Survivor", "Respawn Entertainment", "2023", "Action-adventure, Third-person combat, Sci-Fi, Story-driven", "8.5", "95h");
+
+const myTopGames = [rdr2, cp2077, lou2, swo, swjs];
+
+createHTMLHeader();
+
+createHtmlForTopGames(myTopGames);
---
+++ src/utils.ts
@@ -0,0 +1,42 @@
+import type { FavGames } from "./models/FavGames";
+
+export const createHtmlForTopGames = (myTopGames: FavGames[]) => {
+    const gameSection = document.getElementById("topGames");
+
+  myTopGames.forEach((game) => {
+    const card = document.createElement("div");
+    const title = document.createElement("h3");
+    const dev = document.createElement("p");
+    const year = document.createElement("p");
+    const genre = document.createElement("p");
+    const rating = document.createElement("p");
+    const hours = document.createElement("p");
+
+    title.innerHTML = game.title;
+    dev.innerHTML = game.developer;
+    year.innerHTML = game.realeaseYear;
+    genre.innerHTML = game.genre;
+    rating.innerHTML = game.myRating;
+    hours.innerHTML = game.hoursPlayed;
+
+    card.appendChild(title);
+    card.appendChild(dev);
+    card.appendChild(year);
+    card.appendChild(genre);
+    card.appendChild(rating);
+    card.appendChild(hours);
+
+    gameSection?.appendChild(card);
+  });
+};
+
+export const createHTMLHeader = () => {
+    const header = document.createElement("header");
+    const h1 = document.createElement("h1");
+
+    h1.className = "myHeader";
+    h1.innerHTML = "TopToGame+";
+
+    document.body.prepend(header);
+    header.appendChild(h1);
+}
